4f51669bc82030689b4f0ebe0dc9416b79b01f22
yuta720 <mryy720@yahoo.co.jp>
2022-01-12 23:12:59 +0900

    💄 scssファイル作成

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    { src: '~/assets/sass/app.scss', lang: 'scss' }
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
