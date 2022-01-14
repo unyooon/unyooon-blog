94cae547d3f7dc70c1c80a05e2cdcc6690051cd7
yuta720 <mryy720@yahoo.co.jp>
2022-01-14 23:27:39 +0900

    ✨ ヘッダー作成

---
+++ components/Button.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="button" @click="onClick">
+    {{ text }}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import { buttonTypeText } from '~/types/utility/buttonType';
+export default Vue.extend({
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    onClick: {
+      type: Function,
+      required: true
+    },
+    type: {
+      type: Number as PropType<keyof typeof buttonTypeText>,
+      required: true
+    }
+  },
+  computed: {
+    style (): string {
+      return buttonTypeText[this.type];
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.button {
+  min-width: 32px;
+  max-height: 40px;
+  display: flex;
+  justify-content: center;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  padding-right: 8px;
+  padding-left: 8px;
+  align-items: center;
+  line-height: 1.5em;
+  font-size: 16px;
+}
+
+.button:hover {
+  color: $primary;
+  cursor: pointer;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="header-container">
+    <div class="header-inner">
+      <h1 class="title" @click="clickHome">
+        山 川 道 場
+      </h1>
+      <div class="button-container">
+        <Button
+          text="Home"
+          :type="0"
+          :on-click="clickHome"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  methods: {
+    clickHome () {
+      this.$router.push('/');
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.header-container {
+  width: 100%;
+  height: 96px;
+  display: flex;
+  justify-content: center;
+}
+
+.header-inner {
+  width: 80%;
+  padding-top: 32px;
+  padding-bottom: 8px;
+  padding-right: 8px;
+  padding-left: 8px;
+  border-bottom: 1px solid $primary;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+}
+
+.title {
+  font-family: $header-title-font;
+}
+
+.title:hover {
+  cursor: pointer;
+}
+
+.button-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
