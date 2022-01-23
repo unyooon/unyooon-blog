1027063d71fec06d1e8ecbd7c646209a330a2d6e
yuta720 <mryy720@yahoo.co.jp>
2022-01-23 14:35:20 +0900

    ✨ ブログ一覧ページファイル作成

---
+++ pages/blog/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    blog list
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
