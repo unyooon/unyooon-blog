6a76896c1be7f8f15925c29c516d558833a691d1
ym-kw <mryy720@yahoo.co.jp>
2022-09-03 16:29:22 +0900

    :adhesive_bandage: ogp画像追加

--- assets/mixins/meta.js
+++ assets/mixins/meta.js
@@ -31,7 +31,7 @@ export default {
         { hid: 'og:title', property: 'og:title', content: this.meta.title },
         { hid: 'og:type', property: 'og:type', content: this.meta.type },
         { hid: 'og:url', property: 'og:url', content: this.meta.url },
-        // { hid: 'og:image', property: 'og:image', content: this.meta.slug },
+        { hid: 'og:image', property: 'og:image', content: this.base + require(`~/assets/picture/icatch/${this.meta.slug}-000.jpg`) },
         { name: 'twitter:title', content: this.meta.title },
         { name: 'twitter:text:title', content: this.meta.title }
       ]
