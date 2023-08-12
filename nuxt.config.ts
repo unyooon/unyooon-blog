42fa58b42a129b9b32369363bdb5e67152245877
unyooon <mryy720@yahoo.co.jp>
2023-08-13 01:28:37 +0900

    🩹 generate周り修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,6 @@
+import fs from 'fs'
+import path from 'path'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
@@ -88,4 +91,70 @@ export default defineNuxtConfig({
   },
 
   content: {},
+
+  hooks: {
+    'nitro:config': async function extendNitroConfig(nitroConfig) {
+      const addRoute = (route: string) => {
+        if (!nitroConfig.prerender?.routes?.includes(route)) {
+          nitroConfig.prerender?.routes?.push(route)
+        }
+      }
+
+      const getFilePaths = async (folderPath: string) => {
+        // フォルダ内のすべてのアイテムを取得
+        const items = fs.readdirSync(folderPath)
+
+        const filePaths: string[] = []
+
+        // 各アイテムについて処理を行う
+        await Promise.all(
+          items.map(async (item) => {
+            // アイテムの絶対パスを取得
+            const itemPath = path.join(folderPath, item)
+
+            // 取得した絶対パスを配列に追加
+            // filePaths.push(itemPath)
+
+            // アイテムがディレクトリかどうかを確認
+            const isDirectory = fs.statSync(itemPath).isDirectory()
+
+            if (isDirectory) {
+              // アイテムがディレクトリの場合は再帰的にそのディレクトリ内のファイルパスを取得
+              const subFolderPaths = await getFilePaths(itemPath)
+
+              // 取得したファイルパスを配列に追加
+              filePaths.push(...subFolderPaths)
+            } else {
+              // publicがtrueの場合のみ、ファイルパスを配列に追加
+              const data = await new Promise<string>((resolve, reject) => {
+                fs.readFile(itemPath, 'utf8', (err, data) => {
+                  if (err) reject(err)
+                  resolve(data)
+                })
+              })
+
+              const isPublic = data.includes('public: true')
+              if (isPublic) {
+                filePaths.push(itemPath)
+              }
+            }
+          })
+        )
+
+        return filePaths
+      }
+
+      const folderPath = './content'
+      const filepaths = await getFilePaths(folderPath)
+      console.log(filepaths)
+
+      filepaths.forEach((filepath) => addRoute(filepath.slice(7).split('.')[0]))
+    },
+  },
+
+  nitro: {
+    prerender: {
+      routes: ['/sitemap.xml'],
+    },
+  },
 })
